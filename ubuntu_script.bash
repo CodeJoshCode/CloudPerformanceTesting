67f62241d408ad4d474cb2aa88f30d42152bf039
CodeJoshCode <44310571+CodeJoshCode@users.noreply.github.com>
2021-10-15 14:33:21 -0500

    Fixed results file name
    Made it a variable like it needed to be

--- ubuntu_script.bash
+++ ubuntu_script.bash
@@ -15,31 +15,31 @@ sudo apt-get -y install ioping
 # time first test
 start=`date +%s`
 # first nonsequential rw test 2G
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=2G --readwrite=randrw --rwmixread=75 | tee results_file
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=2G --readwrite=randrw --rwmixread=75 | tee $results_file
 rm random_read_write.fio
 # finish collecting runtime
 end=`date +%s`
 test_1_runtime=$((end-start))
-echo "fio test 1 runtime: $test_1_runtime" >> results_file
+echo "fio test 1 runtime: $test_1_runtime" >> $results_file
 
 # time second test
 start=`date +%s`
 # second nonsequential rw test 5G
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=5G --readwrite=randrw --rwmixread=75 | tee -a results_file
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=5G --readwrite=randrw --rwmixread=75 | tee -a $results_file
 rm random_read_write.fio
 # finish collecting runtime
 end=`date +%s`
 test_2_runtime=$((end-start))
-echo "fio test 2 runtime: $test_2_runtime" >> results_file
+echo "fio test 2 runtime: $test_2_runtime" >> $results_file
 
 # time disk latency test
 start=`date +%s`
 # ioping disk latency test
-ioping -c 20 . | tee -a results_file
+ioping -c 20 . | tee -a $results_file
 # finish collecting runtime
 end=`date +%s`
 test_3_runtime=$((end-start))
-echo "disk latency test 3 runtime: $test_3_runtime" >> results_file
+echo "disk latency test 3 runtime: $test_3_runtime" >> $results_file
 
 # Install passmark tests
 curl https://www.passmark.com/downloads/pt_linux_x64.zip --output passmark.zip
@@ -50,8 +50,8 @@ unzip -y ./passmark.zip
 start=`date +%s`
 # collect CPU/Mem performance data
 ./pt_linux_x64 -p 1 -i 1 -d 1 -r 3
-cat ./results_all.yml >> ./results_file
+cat ./results_all.yml >> $results_file
 # finish collecting runtime
 end=`date +%s`
 test_4_runtime=$((end-start))
-echo "passmark test 4 runtime: $test_4_runtime" >> results_file
+echo "passmark test 4 runtime: $test_4_runtime" >> $results_file
