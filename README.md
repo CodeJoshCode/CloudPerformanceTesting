0eaa117f9e8d3d0386d096496a8d83eb38abfd2e
root <root@josh-host-test-2.localdomain>
2021-10-20 11:36:22 -0500

    results go to nextcloud, file name readable

--- README.md
+++ README.md
@@ -4,14 +4,19 @@ Script to test the performance of a cloud instance. Testing IOPS, Disk Latency, 
 Currently available for Debian based linux instances, tested on Ubuntu 18.04.
 
 ## Usage
-1. After cloning the repo, get execute priveleges for the script. Command:
-chmod 755 ubuntu_script.bash
+1. Clone the repo. git clone https://github.com/CodeJoshCode/CloudPerformanceTesting.git
 
-2. Run the script and the results will appear in a file called total_test.txt.
+2. Make a file called credentials.txt with three lines:
+USER=nextcloud-username
+PWD=nextcloud-password
+results_file_location_nextcloud=nextcloud-folder-location
+TODO: examples
+
+3. Run the script and the results will appear in a file labeled with a unix timestamp of when the test ran.
 
 That's all!
 My next steps are getting output options such as csv, command parameters to tweak the tests, and writing/testing scripts for other Operating Systems.
-TODO: Add a timer to collect the time it takes to run fio and passmark tests.
+TODO: Make the timer output readable dates/times.
 
 Cheers,
 Josh
--- ubuntu_script.bash
+++ ubuntu_script.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
-# sed -i -e 's/\r$//' ./test_script.bash
-# ^to linuxify this script - head -1 ./test_script.bash
+
+# get credentials
+. "$(dirname "$0")"/credentials.txt
 
 # generate results file name
-start=`date +%s`
-location=/root/testing/CloudPerformanceTesting/
-results_file=$location"$start".txt
+start=`date +%m_%d_%y__%H_%M%P`
+location=$(pwd)
+results_file_location=$location"$start".txt
+results_file="$start.txt"
 
 # install fio and run the io tests for 2Gib and 5Gib.
 sudo apt-get -y update
@@ -15,43 +17,48 @@ sudo apt-get -y install ioping
 # time first test
 start=`date +%s`
 # first nonsequential rw test 2G
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=2G --readwrite=randrw --rwmixread=75 | tee $results_file
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=2G --readwrite=randrw --rwmixread=75 | tee $results_file_location
 rm random_read_write.fio
 # finish collecting runtime
 end=`date +%s`
 test_1_runtime=$((end-start))
-echo "fio test 1 runtime: $test_1_runtime" >> $results_file
+echo "fio test 1 runtime: $test_1_runtime" >> $results_file_location
 
 # time second test
 start=`date +%s`
 # second nonsequential rw test 5G
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=5G --readwrite=randrw --rwmixread=75 | tee -a $results_file
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=5G --readwrite=randrw --rwmixread=75 | tee -a $results_file_location
 rm random_read_write.fio
 # finish collecting runtime
 end=`date +%s`
 test_2_runtime=$((end-start))
-echo "fio test 2 runtime: $test_2_runtime" >> $results_file
+echo "fio test 2 runtime: $test_2_runtime" >> $results_fil_locatione
 
 # time disk latency test
 start=`date +%s`
 # ioping disk latency test
-ioping -c 20 . | tee -a $results_file
+ioping -c 20 . | tee -a $results_file_location
 # finish collecting runtime
 end=`date +%s`
 test_3_runtime=$((end-start))
-echo "disk latency test 3 runtime: $test_3_runtime" >> $results_file
+echo "disk latency test 3 runtime: $test_3_runtime" >> $results_file_location
 
 # Install passmark tests
 curl https://www.passmark.com/downloads/pt_linux_x64.zip --output passmark.zip
 sudo apt install -y unzip
-unzip -y ./passmark.zip
+unzip -o ./passmark.zip
 
 # time passmark tests
 start=`date +%s`
 # collect CPU/Mem performance data
 ./pt_linux_x64 -p 1 -i 1 -d 1 -r 3
-cat ./results_all.yml >> $results_file
+cat ./results_all.yml >> $results_file_location
 # finish collecting runtime
 end=`date +%s`
 test_4_runtime=$((end-start))
-echo "passmark test 4 runtime: $test_4_runtime" >> $results_file
+echo "passmark test 4 runtime: $test_4_runtime" >> $results_file_location
+
+
+# Send to NextCloud (TODO: Make optional with credentials file a required paramenter.
+# In case of failure, save test results locally)
+curl -u $USER:$PWD "$results_file_location_nextcloud$results_file" -X PUT -H "Content-Type:text/plain" --data-binary "@$results_file_location"
